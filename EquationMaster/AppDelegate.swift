c525afa3d12c6b79e1b744107f46d4ffe91e5ed4
Stepan Pazderka <stepan.pazderka@icloud.com>
2023-05-12 22:04:44 +0200

    Initial Commit

---
+++ EquationMaster/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  EquationMaster
+//
+//  Created by Štěpán Pazderka on 12.05.2023.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
